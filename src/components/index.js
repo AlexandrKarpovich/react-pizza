ceef5bb0ac4fdd171cddb23d511cfb842b372661
Alex Karpovich <akarpovich@b2broker.net>
2021-04-14 23:43:43 +0300

    4 lesson

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,5 @@
-// import  Header from './Header';
-// import Button from "./Button";
-// import Categories from "./Categories";
-
-// export { Header, Button, Categories };
-
-
 export { default as Button } from './Button';
 export { default as Header } from './Header';
 export { default as Categories } from './Categories';
+export { default as SortPopup } from './SortPopup';
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './scss/app.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { BrowserRouter, Route} from 'react-router-dom';
+
+
+import './scss/app.scss';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+   <BrowserRouter>
+      <Route path="/" component={ App } />
+   </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
